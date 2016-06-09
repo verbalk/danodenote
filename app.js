4d9f2707d7b54b28b62d0a5eac3f1b3323dbcaf3
taylork <verbalk89@naver.com>
2016-06-09 15:11:41 +0900

    del routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cool = require('cool-ascii-faces');
 var socketio = require("socket.io");
-var routes 	= require('./routes');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
