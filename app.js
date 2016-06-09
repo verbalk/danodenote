6d1b484b038f045ea38dcd4dfe5bb21ad4f00077
taylork <verbalk89@naver.com>
2016-06-09 15:52:47 +0900

    app.js update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var session = require('express-session');
 
 var path = require('path');
 
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -20,6 +21,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get('/', routes.index);
+
+/*
 app.get('/', function(request, response) {
   response.render('pages/index')
 });
@@ -27,7 +31,7 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
   response.send(cool());
 });
-
+*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
