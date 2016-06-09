eb3032859c34c0e3185b9c981b405eaee47280d3
taylork <verbalk89@naver.com>
2016-06-09 18:53:37 +0900

    d

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
-
-exports.login = function(req, res){
-
-	
-	res.render("pages/login", ejs_value);
-};
-
-exports.index = function(req, res){
+exports.index = function(req, res,  next){
 
 	
-	res.render("pages/index", ejs_value);
+	res.render('pages/index', { 
+		  title: 'main' 
+		});
 };
-
-
-
-
-
-
-
 
 
