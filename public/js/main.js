f1e27756aaa0234bb7e064d8f64b18b743008363
taylork <verbalk89@naver.com>
2016-06-14 14:51:37 +0900

    socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,7 @@ $(function() {
   var lastTypingTime;
   var $currentInput = $usernameInput.focus();
 
-  var socket = io();
+  var socket = io('http://localhost:5000')
 
   function addParticipantsMessage (data) {
     var message = '';
