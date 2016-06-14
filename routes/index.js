25cb7475fb4f1658aa3db732120667d91bb7d858
taylork <verbalk89@naver.com>
2016-06-14 11:48:50 +0900

    route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,56 @@
+var path 					= require("path");
+var fs						= require('fs');
+var ejs 					= require('ejs');
+var url 					= require('url');
+var async					= require('async');
+var mime					= require('mime');
+
+var UserManager		= require(path.join(__dirname, './../custom_module/user.js'));
+
+
+var tmpdir			= path.join(__dirname, "../upload_file");
+
+var download_tmp_dir				= path.join(__dirname, "../download_tmp");
+
+exports.login = function(req, res){
+	res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+	res.header("Pragma", "no-cache");
+	res.header("Expires", 0);
+	
+	var params = url.parse(req.url, true).query;
+	
+	var ejs_value = {page:"Login"};
+	ejs_value.user = req.session.user;
+	
+	if (params.timeout) {
+		ejs_value.timeout = true;
+	}
+	
+	res.render("pages/login", ejs_value);
+};
+
+exports.index = function(req, res){
+	res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+	res.header("Pragma", "no-cache");
+	res.header("Expires", 0);
+	
+	
+	var ejs_value = {page:"index"};
+	ejs_value.user = req.session.user;
+	
+	res.render("pages/index", ejs_value);
+};
+
+
+
+
+
+
+
+/*
 exports.index = function(req, res,  next){
 	res.render('pages/index', { 
 		  title: 'main' 
 		});
 };
-
-
+*/
