5629df3c0c9c1a4a1bac68b52c36f11ef3a23b0f
taylork <verbalk89@naver.com>
2016-06-14 11:38:44 +0900

    module

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var session = require('express-session'); //login session
 
 var authentication		= require(path.join(__dirname, './custom_module/authentication.js'));
 var system_event		= require(path.join(__dirname, './event_module/events_module.js'));
-var dbdir				= path.join(__dirname, "/database");
+//var dbdir				= path.join(__dirname, "/database");
 var morgan = require("morgan");
 var winston = require('winston');
 
